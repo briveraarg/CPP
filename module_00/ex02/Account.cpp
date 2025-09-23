9b4d13f600d368ea0d606ca9437f0ae7ef2554f9
briveraarg <brivera@student.42madrid.com>
2025-09-23 18:22:53 +0200

    ex02 constructor

---
+++ module_00/ex02/Account.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 17:46:52 by brivera           #+#    #+#             */
+/*   Updated: 2025/09/23 18:13:56 by brivera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	
+	_nbAccounts++;
+	_totalNbDeposits += initial_deposit;
+
+	//[19920104_091532] index:7;amount:16576;created
+}
+
+Account:: ~Account(void)
+{
+}
