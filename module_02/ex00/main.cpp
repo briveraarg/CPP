6f11e25fb58b8ad22d755344565d16a9ea55ab87
briveraarg <brivera@student.42madrid.com>
2025-10-23 14:43:34 +0200

    save

--- module_02/ex00/main.cpp
+++ module_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 18:00:07 by brivera           #+#    #+#             */
-/*   Updated: 2025/10/22 20:13:33 by brivera          ###   ########.fr       */
+/*   Updated: 2025/10/23 14:10:05 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,29 @@
 int main(void)
 {
 	Fixed	a;
-	// Para que 'a' represente el valor 5.0 (5 * 256 = 1280)
-	a.setRawBits(1280);
-	
-	Fixed	b(a);  // b se construye como copia de a
-	Fixed	c;     // c se construye con valor 0
-	
-	std::cout << "Valores iniciales:" << std::endl;
+	Fixed	b(a);
+	Fixed	c;
+		
+	std::cout << BRIGHT_GREEN << "Valores iniciales:" << RESET << std::endl;
 	std::cout << "a: " << a.getRawBits() << std::endl;
 	std::cout << "b: " << b.getRawBits() << std::endl;
 	std::cout << "c: " << c.getRawBits() << std::endl;
 	
-	// Modificar a no afecta a b (son independientes)
-	a.setRawBits(512);  // Cambiar a a 2.0
-	std::cout << "\nDespués de modificar a:" << std::endl;
-	std::cout << "a: " << a.getRawBits() << " (cambiado a 2.0)" << std::endl;
-	std::cout << "b: " << b.getRawBits() << " (sigue siendo 5.0)" << std::endl;
-	
-	// c = b copia el valor de b a c
-	c = b;
-	std::cout << "\nDespués de c = b:" << std::endl;
+	std::cout << BRIGHT_MAGENTA << "Modificamos el valor de a" << RESET << std::endl;
+	a.setRawBits(2855);
+	std::cout << "a: " << a.getRawBits() << std::endl;
 	std::cout << "b: " << b.getRawBits() << std::endl;
-	std::cout << "c: " << c.getRawBits() << " (ahora igual a b)" << std::endl;
 	
-	// Modificar b no afecta a c (son independientes después de la asignación)
-	b.setRawBits(768);  // Cambiar b a 3.0
-	std::cout << "\nDespués de modificar b:" << std::endl;
-	std::cout << "b: " << b.getRawBits() << " (cambiado a 3.0)" << std::endl;
-	std::cout << "c: " << c.getRawBits() << " (sigue siendo 5.0)" << std::endl;
+	std::cout << BRIGHT_CYAN << "Modificamos el valor de c = b" << RESET << std::endl;
+	c = b;
+	std::cout << "a: " << a.getRawBits() << std::endl;
+	std::cout << "b: " << b.getRawBits() << std::endl;
+	std::cout << "c: " << c.getRawBits() << std::endl;
 	
+	std::cout << BRIGHT_RED << "Modificamos el valor de b" << RESET << std::endl;
+	b.setRawBits(768);
+	std::cout << "a: " << a.getRawBits() << std::endl;
+	std::cout << "b: " << b.getRawBits() << std::endl;
+	std::cout << "c: " << c.getRawBits() << std::endl;
 	return (0);
 }
