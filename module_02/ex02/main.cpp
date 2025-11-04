95a2237e241fe044aece06ae6b5d08f82dda8644
briveraarg <brivera@student.42madrid.com>
2025-11-04 19:24:05 +0100

    mejora del main

--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:19:25 by brivera           #+#    #+#             */
-/*   Updated: 2025/11/04 15:57:12 by brivera          ###   ########.fr       */
+/*   Updated: 2025/11/04 19:18:42 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,22 @@ int main( void )
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << BRIGHT_CYAN << BOLD << "---- ex02 ----" << RESET << std::endl;
+	std::cout << BRIGHT_MAGENTA  << "valor de a: " << RESET;
 	std::cout << a << std::endl;
+	std::cout << BRIGHT_MAGENTA  << "valor de a (pre-incremento): " << RESET;
 	std::cout << ++a << std::endl;
+	std::cout << BRIGHT_MAGENTA  << "valor de a: " << RESET;
 	std::cout << a << std::endl;
+	std::cout << BRIGHT_MAGENTA  << "valor de a (pos-incremento): " << RESET;
 	std::cout << a++ << std::endl;
+	std::cout << BRIGHT_MAGENTA  << "valor de a: " << RESET;
 	std::cout << a << std::endl;
+
+	std::cout << BRIGHT_GREEN  << "valor de b: " << RESET;
 	std::cout << b << std::endl;
+	std::cout << BRIGHT_BLUE  << "valor MAX: " << RESET;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << BRIGHT_BLUE  << "valor MIN: " << RESET;
+	std::cout << Fixed::min( a, b ) << std::endl;
 	return 0;
 }
