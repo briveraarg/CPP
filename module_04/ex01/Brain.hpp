230ba198c5bd644efab5c121c1a9203069498a57
briveraarg <brivera@student.42madrid.com>
2025-11-27 19:02:14 +0100

    comentario de la clase

--- module_04/ex01/Brain.hpp
+++ module_04/ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:06:00 by brivera           #+#    #+#             */
-/*   Updated: 2025/11/27 17:27:29 by brivera          ###   ########.fr       */
+/*   Updated: 2025/11/27 19:01:31 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 
 #include <iostream>
 #include "color.hpp"
+
+/*
+ * 
+ * Estructura de la clase Brain:
+ * - Atributo privado:
+ * 		std::string _ideas[100]; // array de 100 strings para almacenar ideas.
+ * - Constructores:
+ * 		Brain() -> constructor por defecto (inicializa ideas).
+ * 		Brain(const Brain& other) -> constructor de copia (copia el array de ideas).
+ * - Operador de asignación: 
+ * 		operator=(const Brain& other) copia el contenido del array de ideas.
+ * - Destructor:
+ * 		~Brain() destructor de la clase.
+ * - Métodos:
+ * 		getIdeas(index) -> devuelve la idea en la posición index.
+ * 		setIdea(index, idea) -> establece una idea en la posición index.
+ * 
+*/
 
 class	Brain
 {
