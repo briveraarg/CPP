ca43c5cd897a64788d99233d95e6c2a9969ff6f7
briveraarg <brivera@student.42madrid.com>
2025-11-29 20:08:00 +0100

    hpp materia source

--- module_04/ex03/MateriaSource.hpp
+++ module_04/ex03/MateriaSource.hpp
@@ -6,28 +6,33 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 17:11:21 by brivera           #+#    #+#             */
-/*   Updated: 2025/11/29 17:12:19 by brivera          ###   ########.fr       */
+/*   Updated: 2025/11/29 20:05:01 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATERIASOURCE_HPP
 #define MATERIASOUCE_HPP
 
-class MateriaSource
+#include "IMateriaSource.hpp"
+#include "AMateria.hpp"
+
+#define MAX_MATERIA 4
+
+class MateriaSource : public IMateriaSource
 {
 	private:
-		/* data */
+		AMateria*	_materias[MAX_MATERIA];
+
 	public:
-		MateriaSource(/* args */);
+		MateriaSource(void);
+		MateriaSource(const MateriaSource& other);
 		~MateriaSource();
-	};
 
-MateriaSource::MateriaSource(/* args */)
-{
-}
-
-MateriaSource::~MateriaSource()
-{
-}
+		MateriaSource& operator=(const MateriaSource& other);
+		
+		void		learnMateria(AMateria*);
+		AMateria*	createMateria(std::string const & type);
+		
+};
 
 #endif
