c2c0561f41bb60088c5a0f2ff9f4da3ec6cff958
briveraarg <brivera@student.42madrid.com>
2025-12-03 18:44:02 +0100

    inicios del m5

---
+++ module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 15:36:54 by brivera           #+#    #+#             */
+/*   Updated: 2025/12/03 16:49:52 by brivera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+/***************************************/
+/*                COLOR                */
+/***************************************/
+
+#define RESET			"\033[0m"
+#define BRIGHT_RED		"\033[91m"
+#define BRIGHT_GREEN	"\033[92m"
+#define BRIGHT_BLUE		"\033[94m"
+#define BRIGHT_MAGENTA	"\033[95m"
+#define BRIGHT_CYAN		"\033[96m"
+
+#define BOLD			"\033[1m"
+
+class Bureaucrat
+{
+	private:
+		std
+	public:
+		Bureaucrat(/* args */);
+		~Bureaucrat();
+};
+
+
+
+
+
+#endif
