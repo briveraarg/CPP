82a32100a61611acd1e4101963da163a23610e7c
briveraarg <brivera@student.42madrid.com>
2025-12-15 17:13:37 +0100

    guardar

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 16:57:28 by brivera           #+#    #+#             */
-/*   Updated: 2025/12/15 17:01:56 by brivera          ###   ########.fr       */
+/*   Updated: 2025/12/15 17:13:06 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 {
 	if (argc > 2)
 	{
-		std::cerr << "Error\n Uso: ./ex00 a" << std::endl;
+		std::cerr << "Error\n Uso: ./convert a" << std::endl;
 		return (1);
 	}
 	std::string valor = argv[1];
