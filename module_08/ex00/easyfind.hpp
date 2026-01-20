4fe69cc5deab6900685731d3ea2ae2cff0098413
briveraarg <brivera@student.42madrid.com>
2026-01-20 13:18:49 +0100

    save

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 12:43:07 by brivera           #+#    #+#             */
+/*   Updated: 2026/01/20 13:17:13 by brivera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <algorithm>
+#include <exception>
+
+//template <typename T>
+
+#include "easyfind.tpp"
+#endif
