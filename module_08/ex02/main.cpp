baede43d9c3aa3203c3e2487c8081bae376f78cf
briveraarg <brivera@student.42madrid.com>
2026-01-23 15:30:22 +0100

    save

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:20:34 by brivera           #+#    #+#             */
-/*   Updated: 2026/01/23 15:02:31 by brivera          ###   ########.fr       */
+/*   Updated: 2026/01/23 15:29:33 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,62 +18,87 @@
 #define BRIGHT_BLUE		"\033[94m"
 #define BOLD			"\033[1m"
 
+#include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
+#define BOLD        "\033[1m"
+#define BRIGHT_BLUE "\033[94m"
+#define RESET       "\033[0m"
+
 int main()
 {
-	std::cout << BOLD << BRIGHT_BLUE
-		<< "=== TEST MUTANT STACK (Subject) ===" << RESET << std::endl;
-	
-	MutantStack<int>	mstack;
-	
-	mstack.push(5);
-	mstack.push(17);
-	
-	std::cout << "Top: " << mstack.top() << std::endl;
-	
-	mstack.pop();
-	
-	std::cout << "Size: " << mstack.size() << std::endl;
-	
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-	
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	
-	++it;
-	--it;
-	
-	std::cout << BOLD << "\nIterando MutantStack..."
-		<< RESET << std::endl;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
-	std::stack<int> s(mstack);
+    std::cout << BOLD << BRIGHT_BLUE
+              << "=== TEST MUTANT STACK (Subject) ===" << RESET << std::endl;
 
-	std::cout << BOLD << BRIGHT_BLUE
-		<< "\n=== TEST LIST COMPARISON ===" << RESET << std::endl;
-	
-	std::list<int> l;
-	l.push_back(5);
-	l.push_back(17);
-	l.pop_back();
-	l.push_back(3);
-	l.push_back(5);
-	l.push_back(737);
-	l.push_back(0);
+    MutantStack<int> mstack;
 
-	std::list<int>::iterator itb_l = l.begin();
-	std::list<int>::iterator ite_l = l.end();
+    mstack.push(5);
+    mstack.push(17);
 
-	std::cout << BOLD << "Iterando std::list: " << RESET << std::endl;
-	while (itb_l != ite_l)
-	{
-		std::cout << *itb_l << std::endl;
-		itb_l++;
-	}
-	return 0;
+    std::cout << "Top: " << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << "Size: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    // Iterador normal
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+
+    std::cout << BOLD << "\nIterando MutantStack..." << RESET << std::endl;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    // Iterador constante (ejemplo)
+    const MutantStack<int> cmstack = mstack;
+    MutantStack<int>::const_iterator cit = cmstack.begin();
+    MutantStack<int>::const_iterator cite = cmstack.end();
+
+    std::cout << BOLD << "\nIterando MutantStack con const_iterator..." << RESET << std::endl;
+    while (cit != cite)
+    {
+        std::cout << *cit << std::endl;
+        ++cit;
+    }
+
+    // Comparación con std::stack
+    std::stack<int> s(mstack);
+
+    std::cout << BOLD << BRIGHT_BLUE
+              << "\n=== TEST LIST COMPARISON ===" << RESET << std::endl;
+
+    std::list<int> l;
+    l.push_back(5);
+    l.push_back(17);
+    l.pop_back();
+    l.push_back(3);
+    l.push_back(5);
+    l.push_back(737);
+    l.push_back(0);
+
+    std::list<int>::iterator itb_l = l.begin();
+    std::list<int>::iterator ite_l = l.end();
+
+    std::cout << BOLD << "Iterando std::list: " << RESET << std::endl;
+    while (itb_l != ite_l)
+    {
+        std::cout << *itb_l << std::endl;
+        ++itb_l;
+    }
+
+    return 0;
 }
+
+
